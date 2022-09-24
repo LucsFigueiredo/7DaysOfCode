b5e69d412930abab811604094d97805153106e32
LucasFS <96503815+LucsFigueiredo@users.noreply.github.com>
2022-09-24 12:35:22 -0300

    PopularMovies - Teste com API - dia 4

--- PopularMovies - Teste com API/script.js
+++ PopularMovies - Teste com API/script.js
@@ -1,69 +1,112 @@
-const filmes = [
-    {
-        image: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
-        title: 'Batman',
-        rating: 9.2,
-        year: 2022,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: true,
-    },
-    {
-        image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
-        title: 'Avengers',
-        rating: 9.2,
-        year: 2019,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: false
-    },
-    {
-        image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
-        title: 'Doctor Strange',
-        rating: 9.2,
-        year: 2022,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: false
-    },
-]
+import { apiKey } from "./config/api-key.js"
 
-const filmesElemento = document.querySelector(".filmes");
+async function getPopularMovies() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+    const fetchResponse = await fetch(url)
+    const { results } = await fetchResponse.json()
+    return results
+} 
 
-window.onload = function() {
-    filmes.forEach(filme => renderizarFilmes(filme))
+window.onload = async function() {
+    const movies = await getPopularMovies()
+    movies.forEach(movie => renderMovies(movie))
 }
 
-function renderizarFilmes(filme) {
-    const { title, image, rating, year, description } = filme
 
-    const filmesDiv = document.createElement("div")
-    const filmesImgDiv = document.createElement('div')
-    const filmesImg = document.createElement("img")
-    const filmesDivInfo = document.createElement("div")
-    const filmesTitulo = document.createElement("p")
-    const filmesDivDados = document.createElement("div")
-    const filmesRating = document.createElement("p")
-    const filmesFavoritar = document.createElement("p")
-    const filmesP = document.createElement("p")
+const moviesContainer = document.querySelector(".movies");
 
-    filmesTitulo.textContent = title + " " + "(" + year + ")"
-    filmesImg.src = image
-    filmesP.textContent = description
-    filmesRating.textContent = "⭐" + " " + rating
-    filmesFavoritar.textContent = "💖 Favoritar"
 
-    filmesDiv.classList.add("filme")
-    filmesImgDiv.classList.add('filmeImage')
-    filmesDivInfo.classList.add("filmeInfo")
-    filmesDivDados.classList.add("filmeDados")
-    filmesTitulo.classList.add("filmeTitulo")
-    filmesP.classList.add("descricao")
+function renderMovies(movie) {
+    const { title, poster_path, vote_average, release_date, overview } = movie
+    const isFavorited = false
 
-    filmesElemento.appendChild(filmesDiv)
-    filmesDiv.appendChild(filmesImgDiv)
-    filmesDiv.appendChild(filmesDivInfo)
-    filmesDiv.appendChild(filmesP)
-    filmesImgDiv.appendChild(filmesImg)
-    filmesDivInfo.appendChild(filmesTitulo)
-    filmesDivInfo.appendChild(filmesDivDados)
-    filmesDivDados.appendChild(filmesRating)
-    filmesDivDados.appendChild(filmesFavoritar)
+    const year = new Date(release_date).getFullYear()
+    const image = `https://image.tmdb.org/t/p/w500${poster_path}`
+
+    const movieElement = document.createElement('div')
+    const movieInformations = document.createElement('div')
+    const movieImageContainer = document.createElement('div')
+    const movieImage = document.createElement('img')
+    const movieTextContainer = document.createElement('div')
+    const movieTitle = document.createElement('h4')
+    const informations = document.createElement('div')
+    const ratingContainer = document.createElement('div')
+    const starImage = document.createElement('img')
+    const movieRate = document.createElement('span')
+    const favorite = document.createElement('div')
+    favorite.classList.add('favorite')
+    const favoriteImage = document.createElement('img')
+    const favoriteText = document.createElement('span')
+    const movieDescriptionContainer = document.createElement('div')
+    const movieDescription = document.createElement('span')
+
+
+    movieElement.classList.add('movie')
+    movieInformations.classList.add('movie-informations')
+    movieImageContainer.classList.add('movie-image')
+    movieTextContainer.classList.add('movie-text')
+    informations.classList.add('movie-informations')
+    ratingContainer.classList.add('rating')
+    movieRate.classList.add('movie-rate')
+    favoriteImage.classList.add('favoriteImage')
+    favoriteText.classList.add('movie-favorite')
+    movieDescriptionContainer.classList.add('movie-description')
+
+
+    moviesContainer.appendChild(movieElement)
+    movieImageContainer.appendChild(movieImage)
+    movieInformations.appendChild(movieImageContainer)
+    movieTextContainer.appendChild(movieTitle)
+    movieInformations.appendChild(movieTextContainer)
+    movieTextContainer.appendChild(informations)
+    ratingContainer.appendChild(starImage)
+    ratingContainer.appendChild(movieRate)
+    informations.appendChild(ratingContainer)
+    favorite.appendChild(favoriteImage)
+    favorite.appendChild(favoriteText)
+    informations.appendChild(favorite)
+    movieDescriptionContainer.appendChild(movieDescription)
+    movieElement.appendChild(movieInformations)
+    movieElement.appendChild(movieDescriptionContainer)
+
+
+    movieImage.src = image
+    movieImage.alt = `${title} Poster`
+    movieTitle.textContent = `${title} (${year})`
+    starImage.src = 'images/star.png'
+    starImage.alt = 'Star'
+    movieRate.textContent = vote_average
+    favoriteImage.src = isFavorited ? 'images/heart-fill.svg' : 'images/heart.svg'
+    favoriteImage.alt = 'Heart'
+    favoriteText.textContent = 'Favoritar'
+    movieDescription.textContent = overview
+    
 }
+
+document.querySelector("#movie-name").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+    search()
+    }
+})
+
+document.querySelector("#searchIcon").addEventListener("click", search)
+
+async function search() {
+    const inputValue = document.getElementById("movie-name").value
+    if (inputValue != '') {
+      cleanAllMovies()
+      const movies = await searchMovieByName(inputValue)
+      movies.forEach(movie => renderMovies(movie))
+    }
+  }
+  
+  function cleanAllMovies() {
+    moviesContainer.innerHTML = ''
+  }
+  
+  async function searchMovieByName(inputValue) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${inputValue}&language=en-US&page=1`
+    const fetchResponse = await fetch(url)
+    const { results } = await fetchResponse.json()
+    return results
+  }
